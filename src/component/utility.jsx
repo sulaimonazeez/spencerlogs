8e9fe7c2781e0d4d995a804be363f6ff2e8ee6a8
sulaimonazeez <olaniyisulaimon221@gmail.com>
2025-08-12 03:19:47 -0800

    commited

--- src/component/utility.jsx
+++ src/component/utility.jsx
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'http://127.0.0.1:8000';
+const API_BASE_URL = 'https://logserver.pythonanywhere.com';
 
 const axiosInstance = axios.create({
   baseURL: API_BASE_URL,
